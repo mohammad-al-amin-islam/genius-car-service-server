59c16d4140840126c166f71e0399b87400c03f6d
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-26 12:43:19 +0600

    insert service to db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -21,11 +21,25 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("service");
 
+        //load all data from db
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        //load single data from db
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+        app.post('/serviceadd', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result)
         })
     }
     finally {
