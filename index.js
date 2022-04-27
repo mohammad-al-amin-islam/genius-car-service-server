520f4bef0d45e463b588f78db61329cd16121c47
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-04-27 08:45:47 +0600

    delete from db implemented

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result)
+        });
+
+        //delete 
+        app.delete('/manageservice/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
